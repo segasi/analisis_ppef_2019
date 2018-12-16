317766ad347cec784ecd06a08430986a6fe54de8
segasi <segasi@gmail.com>
2018-12-15 21:04:06 -0600

    Importar datos y cargar readxl

--- 02_codigo/analisis_ppef_2019.R
+++ 02_codigo/analisis_ppef_2019.R
@@ -1,6 +1,6 @@
 ### Paquetes ----
 library(pacman)
-p_load(ggrepel, scales, tidyverse, treemapify)
+p_load(ggrepel, scales, readxl, tidyverse, treemapify)
 
 ### Setup ----
 Sys.setlocale("LC_ALL", "es_ES.UTF-8") # Cambiar locale para prevenir problemas con caracteres especiales
@@ -10,7 +10,10 @@ options(scipen=999) # Prevenir notación científica
 
 # Los datos los obtuve de las secciones ¿PARA QUÉ SE GASTA? y ¿QUIÉN GASTA? en esta liga: https://www.transparenciapresupuestaria.gob.mx/es/PTP/infografia_ppef2019#page3
 
+# Datos de ¿Para qué se gasta?
+bd_para_que <- read_xlsx("01_datos/bd_para_que_se_gasta.xlsx")
 
-
+# Datos de ¿Quién gasta?
+bd_quien <- read_xlsx("01_datos/bd_quien_gasta.xlsx")
 
 
