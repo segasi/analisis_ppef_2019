bb5f439fd08f79413ec304442ff77d180a1165e5
segasi <segasi@gmail.com>
2018-12-15 20:48:30 -0600

    Setup

--- 02_codigo/analisis_ppef_2019.R
+++ 02_codigo/analisis_ppef_2019.R
@@ -1,3 +1,7 @@
 ### Paquetes ----
 library(pacman)
 p_load(ggrepel, scales, tidyverse, treemapify)
+
+### Setup ----
+Sys.setlocale("LC_ALL", "es_ES.UTF-8") # Cambiar locale para prevenir problemas con caracteres especiales
+options(scipen=999) # Prevenir notación científica
