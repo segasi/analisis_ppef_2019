eca659cfb58ba7b3e19c3acac7d07312a3dc1b4a
segasi <segasi@gmail.com>
2018-12-16 07:53:19 -0600

    Cargar PPEF

--- 02_codigo/analisis_ppef_2019.R
+++ 02_codigo/analisis_ppef_2019.R
@@ -1,6 +1,6 @@
 ### Paquetes ----
 library(pacman)
-p_load(ggrepel, scales, readxl, tidyverse, treemapify)
+p_load(ggrepel, janitor, scales, readxl, tidyverse, treemapify)
 
 ### Setup ----
 Sys.setlocale("LC_ALL", "es_ES.UTF-8") # Cambiar locale para prevenir problemas con caracteres especiales
@@ -25,11 +25,16 @@ tema <-  theme_minimal() +
 
 # Los datos los obtuve de las secciones ¿PARA QUÉ SE GASTA? y ¿QUIÉN GASTA? en esta liga: https://www.transparenciapresupuestaria.gob.mx/es/PTP/infografia_ppef2019#page3
 
+# PPEF 2019 completo
+ppef <- read_csv("01_datos/PPEF_2019.csv", locale = locale(encoding = "latin1"))
+
 # Datos de ¿Para qué se gasta?
 bd_para_que <- read_xlsx("01_datos/bd_para_que_se_gasta.xlsx")
 
 # Datos de ¿Quién gasta?
 bd_quien <- read_xlsx("01_datos/bd_quien_gasta.xlsx")
+
+
 
 ### Transformar datos ----
 
@@ -48,5 +53,7 @@ bd_ramo <- bd_quien %>%
          -c(ramo, tipo, var_real)) %>% 
   mutate(año = ifelse(fuente == "proyecto_2019", 2019, 2018),
          var_real = ifelse(año == 2018, 0, var_real)) 
+
+
 
 
