12c0a6a2a5ae9cc03bf5c7d6668206889dbf0be2
segasi <segasi@gmail.com>
2018-12-15 21:03:47 -0600

    Importar datos agregados y cargar readxl

--- 02_codigo/analisis_ppef_2019.R
+++ 02_codigo/analisis_ppef_2019.R
@@ -5,3 +5,12 @@ p_load(ggrepel, scales, tidyverse, treemapify)
 ### Setup ----
 Sys.setlocale("LC_ALL", "es_ES.UTF-8") # Cambiar locale para prevenir problemas con caracteres especiales
 options(scipen=999) # Prevenir notación científica
+
+### Datos ----
+
+# Los datos los obtuve de las secciones ¿PARA QUÉ SE GASTA? y ¿QUIÉN GASTA? en esta liga: https://www.transparenciapresupuestaria.gob.mx/es/PTP/infografia_ppef2019#page3
+
+
+
+
+
